466ff30e94a4d2489dff6eaf53e5807854a1cbd4
Alisa Vasilenko <alisa314159@gmail.com>
2024-04-19 21:38:17 +0200

    upd template

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import Modal from './components/Modal/Modal'
 import Backdrop from './components/Backdrop/Backdrop'
 import List from './components/List/List'
 
+// Packages for animation:
+// 1. react-transition-group  https://www.npmjs.com/package/react-transition-group
+// 2. react-motion  https://github.com/chenglou/react-motion
+// 3. react-move  https://github.com/sghall/react-move
+// 4. react-router-transition  https://github.com/maisano/react-router-transition - for animating routing process
+
 class App extends Component {
   state = {
     modalIsOpen: false,
@@ -21,14 +27,8 @@ class App extends Component {
     return (
       <div className="app">
         <h1>React Animations</h1>
-        <Modal
-          show={this.state.modalIsOpen}
-          closed={this.closeModal}
-        />
-        <Backdrop
-          show={this.state.modalIsOpen}
-          closed={this.closeModal}
-        />
+        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
+        <Backdrop show={this.state.modalIsOpen} closed={this.closeModal} />
         <button className="button" onClick={this.showModal}>
           Open Modal
         </button>
